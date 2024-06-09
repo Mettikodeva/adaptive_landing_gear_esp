61def65e20040a2a6ef5f2324dd901fe13a75d2e
nara atthama <pancasila.day@gmail.com>
2024-06-10 02:07:32 +0700

    fix: wrong FK parameter, search angle logic, and terrain tilt calculation

--- main/landing_gear.cpp
+++ main/landing_gear.cpp
@@ -16,11 +16,15 @@
 // MARK: MACROS
 // adjust these values to match the servo's calibration values
 
-#define MAP(x,in_min,in_max,out_min,out_max) (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+#define MAP(x,in_min,in_max,out_min,out_max) ((x - in_min) * (out_max - out_min) / (in_max - in_min)) + out_min
 
-#define PWM1(x) MAP(x, 0, 90, 2370, 1050)
-#define PWM2(x) MAP(x, 0, 90, 2460, 1090)
-#define PWM3(x) MAP(x, 0, 90, 2500, 1070)
+//  (90-0)*((1050-2370)/(90)) + 2370
+// -1320
+// 2460
+
+#define PWM1(x) MAP(x, 0, 90, CONFIG_SERVO1_MAX, CONFIG_SERVO1_MIN)
+#define PWM2(x) MAP(x, 0, 90, CONFIG_SERVO2_MAX, CONFIG_SERVO2_MIN)
+#define PWM3(x) MAP(x, 0, 90, CONFIG_SERVO3_MAX, CONFIG_SERVO3_MIN)
 
 
 // MARK: Variables
@@ -38,7 +42,7 @@ TaskHandle_t TaskHandle_terrain;
 TaskHandle_t TaskHandle_ADC;
 
 SemaphoreHandle_t servo_sem = xSemaphoreCreateBinary();
-SemaphoreHandle_t legs_fk_sem = xSemaphoreCreateBinary();
+// SemaphoreHandle_t legs_fk_sem = xSemaphoreCreateBinary();
 // portMUX_TYPE terrainMux = portMUX_INITIALIZER_UNLOCKED;
 // portMUX_TYPE servoMux = portMUX_INITIALIZER_UNLOCKED;
 
@@ -100,13 +104,13 @@ SemaphoreHandle_t lidar_control_mutex = xSemaphoreCreateBinary();
     PID * pid_pitch2 = new PID(kpi, kii, kdi); // imu
 #endif
 
-int base_pwm = 1200; // in microseconds
-int default_angle = 30;
+// int base_pwm = 1200; // in microseconds
+int default_angle = 45;
 int prev_pwm[3] = {0, 0, 0}; // in microseconds
 VectorInt16 legs[3];    // x, y, z
 int pwms[3] = {1200,1200,1200};          // in microseconds
 float terrain_pitch = 0, terrain_roll = 0;
-int target_servo_angle_terrain[3] = {0,0,0};
+float target_servo_angle_terrain[3] = {0,0,0};
 char tolerance = 1; // in degrees
 
 
@@ -136,12 +140,12 @@ float deg2rad(float deg){
 }
 
 float rad2deg(float rad){
-    return rad * 180 / PI;
+    return rad * 180 / M_PI;
 }
 
 VectorFloat getPlaneNorm(VectorInt16 a, VectorInt16 b, VectorInt16 c){
-    VectorInt16 u = {int16_t(b.x - a.x), int16_t(b.y - a.y), int16_t(b.z - a.z)}; 
-    VectorInt16 v = {int16_t(c.x - a.x), int16_t(c.y - a.y), int16_t(c.z - a.z)};
+    VectorFloat u = {float(c.x - a.x), float(c.y - a.y), float(c.z - a.z)};
+    VectorFloat v = {float(b.x - a.x), float(b.y - a.y), float(b.z - a.z)}; 
 
     // normal vector of the plane
     /*  | i   j   k |
@@ -201,11 +205,10 @@ void servoWriteTask(void *pvParameter){
                 m3 += 2*pcval_imu;
                 break;
             case DESCENDING:
-                if(target_servo_angle_terrain[0] == -1|| target_servo_angle_terrain[1] == -1|| target_servo_angle_terrain[2] == -1)
-                    break;
                 m1 = PWM1(target_servo_angle_terrain[0]);
                 m2 = PWM2(target_servo_angle_terrain[1]);
                 m3 = PWM3(target_servo_angle_terrain[2]);
+                break;
             default:
                 break;
             }
@@ -261,10 +264,11 @@ void servoWriteTask(void *pvParameter){
                 break;
             }
 
-    
+            xSemaphoreTake(servo_sem, 10/portTICK_PERIOD_MS);
             servo->writeMicroseconds(0, pwms[0]);
             servo->writeMicroseconds(1, pwms[1]);
             servo->writeMicroseconds(2, pwms[2]);
+            xSemaphoreGive(servo_sem);
     }
 }
 
@@ -324,7 +328,7 @@ void IRAM_ATTR getFK(){
 
     legfx = L1 * sin(deg2rad(getAngle(3)));
     legfy = L1 * cos(deg2rad(getAngle(3)));
-    legs[2].x = (dBase +legfx);
+    legs[2].x = -(dBase +legfx);
     legs[2].y = 0;
     legs[2].z = L2 + legfy + (distance[2] - dToF);
 
@@ -336,53 +340,46 @@ void IRAM_ATTR getFK(){
             legs[i].y = sin(deg2rad(120 * (i) - 60)) * (d + L1*sin(deg2rad(angle)));
             legs[i].z = L2 + L1 * cos(deg2rad(angle)) + (distance[i] - dLidar);    
     */
-    // portEXIT_CRITICAL(&terrainMux);
+    
     ESP_LOGI("FK"," time: %ld",pdTICKS_TO_MS(xTaskGetTickCount() - start));
-    // vTaskNotifyGiveFromISR(TaskHandle_terrain, &xHigherPriorityTaskWoken);
-    // xSemaphoreGiveFromISR(legs_fk_sem, &xHigherPriorityTaskWoken);
-    // if(xHigherPriorityTaskWoken == pdTRUE){
-    //     portYIELD_FROM_ISR();
-    // }
 }
 
-int16_t getDist2Psqrt(VectorInt16 a, VectorInt16 b){
-    return (sq(a.x - b.x) + sq(a.y - b.y) + sq(a.z - b.z));
-}
+// int16_t getDist2Psqrt(VectorInt16 a, VectorInt16 b){
+//     return (sq(a.x - b.x) + sq(a.y - b.y) + sq(a.z - b.z));
+// }
 
-float *getIK(){
-    float *angles = (float *)malloc(3 * sizeof(float));
-    angles[0] = acos((getDist2Psqrt(VectorInt16(dBase*-0.5f,dBase*0.866025f,0),legs[0]) - 16900) / (12000));
-    angles[1] = acos((getDist2Psqrt(VectorInt16(dBase* 0.5f,dBase*0.866025f,0),legs[1]) - 16900) / (12000));
-    angles[2] = acos((getDist2Psqrt(VectorInt16(dBase      ,0              ,0),legs[2]) - 16900) / (12000));
-    /*
-    angles[0] = acos((getDist2P(VectorInt16(dBase*cos(120),dBase*sin(120),0),legs[0]) - sq(L1) - sq(L2)) / (2 * L1 * L2));
-    angles[1] = acos((getDist2P(VectorInt16(dBase*cos(60),dBase*sin(60),0),legs[1]) - sq(L1) - sq(L2)) / (2 * L1 * L2));
-    angles[2] = acos((getDist2P(VectorInt16(dBase*cos(0),dBase*sin(0),0),legs[2]) - sq(L1) - sq(L2)) / (2 * L1 * L2));
-    */
-    return angles;
-}
+// float *getIK(){
+//     float *angles = (float *)malloc(3 * sizeof(float));
+//     angles[0] = acos((getDist2Psqrt(VectorInt16(dBase*-0.5f,dBase*0.866025f,0),legs[0]) - 16900) / (12000));
+//     angles[1] = acos((getDist2Psqrt(VectorInt16(dBase* 0.5f,dBase*0.866025f,0),legs[1]) - 16900) / (12000));
+//     angles[2] = acos((getDist2Psqrt(VectorInt16(dBase      ,0              ,0),legs[2]) - 16900) / (12000));
+//     /*
+//     angles[0] = acos((getDist2P(VectorInt16(dBase*cos(120),dBase*sin(120),0),legs[0]) - sq(L1) - sq(L2)) / (2 * L1 * L2));
+//     angles[1] = acos((getDist2P(VectorInt16(dBase*cos(60),dBase*sin(60),0),legs[1]) - sq(L1) - sq(L2)) / (2 * L1 * L2));
+//     angles[2] = acos((getDist2P(VectorInt16(dBase*cos(0),dBase*sin(0),0),legs[2]) - sq(L1) - sq(L2)) / (2 * L1 * L2));
+//     */
+//     return angles;
+// }
 
-void searchAngleFK(){
-    target_servo_angle_terrain[0] = -1;
-    target_servo_angle_terrain[1] = -1;
-    target_servo_angle_terrain[2] = -1;
+bool searchAngleFK(){
+    // target_servo_angle_terrain[0] = -1;
+    // target_servo_angle_terrain[1] = -1;
+    // target_servo_angle_terrain[2] = -1;
     TickType_t start = xTaskGetTickCount();
-    float angle[3] = {0,0,0};
-    angle[0] = getAngle(1);
-    angle[1] = getAngle(2);
-    angle[2] = getAngle(3);
+    float angle[3] = {45,45,45};
+    // angle[0] = getAngle(1);
+    // angle[1] = getAngle(2);
+    // angle[2] = getAngle(3);
     VectorInt16 tmp_legs[3];
     VectorFloat n;
     vTaskDelay(5 / portTICK_PERIOD_MS);
+    bool flag_found = false;
     while (pdTICKS_TO_MS(xTaskGetTickCount() - start) < 3000)
     {
-        angle[0] = getAngle(1);
-        angle[1] = getAngle(2);
-        angle[2] = getAngle(3);
         float tmp_pitch = 0.0f, tmp_roll = 0.0f;
         int16_t legfx = L1 * sin(deg2rad(angle[0])), legfy = L1 * cos(deg2rad(angle[0]));
-        tmp_legs[0].x = -0.5f * (dBase +legfx);
-        tmp_legs[0].y = 0.866025f * (dBase + legfx);
+        tmp_legs[0].x = 0.5f * (dBase +legfx);
+        tmp_legs[0].y = -0.866025f * (dBase + legfx);
         tmp_legs[0].z = L2 + legfy;
         legfx = L1 * sin(deg2rad(angle[1]));
         legfy = L1 * cos(deg2rad(angle[1]));
@@ -391,16 +388,17 @@ void searchAngleFK(){
         tmp_legs[1].z = L2 + legfy;
         legfx = L1 * sin(deg2rad(angle[2]));
         legfy = L1 * cos(deg2rad(angle[2]));
-        tmp_legs[2].x = (dBase +legfx);
+        tmp_legs[2].x = -(dBase +legfx);
         tmp_legs[2].y = 0;
         tmp_legs[2].z = L2 + legfy;
+
         n = getPlaneNorm(tmp_legs[0], tmp_legs[1], tmp_legs[2]);
         // pitch and roll
         // pitch and roll
         ESP_LOGI("TAG_NORM_SEARCH","%ld, %3.2f, %3.2f, %3.2f",xTaskGetTickCount(), n.x, n.y, n.z);
-        
-        tmp_pitch = rad2deg(atan2(n.x, n.z));
-        tmp_roll = -rad2deg(atan2(n.y, n.z));
+        tmp_pitch = rad2deg(atan((n.x)/(n.z)));
+        tmp_roll = rad2deg(atan((n.y)/(n.z)));
+        printf("pitch: %f, roll: %f\n", tmp_pitch, tmp_roll);
 
 
         // if(terrain_pitch > 90){
@@ -410,31 +408,50 @@ void searchAngleFK(){
         //     terrain_pitch += 180;
         // }
         vTaskDelay(5 / portTICK_PERIOD_MS);
+        if(angle[0] > 90 || angle[0] < 0 || angle[1] > 90 || angle[1] < 0 || angle[2] > 90 || angle[2] < 0){
+            break;
+        }   
         if(abs(tmp_roll-terrain_roll) < 5 && abs(tmp_pitch - terrain_pitch) < 5){
             target_servo_angle_terrain[0] = angle[0];
             target_servo_angle_terrain[1] = angle[1];
             target_servo_angle_terrain[2] = angle[2];
+            flag_found = true;
             break;
         }
-        angle[0] += (terrain_pitch-tmp_pitch) < 0 ? angle[0] + 0.5 : angle[0] - 0.5;
-        angle[1] += (terrain_pitch-tmp_pitch) < 0 ? angle[1] + 0.5 : angle[1] - 0.5;
-        angle[2] -= (terrain_pitch-tmp_pitch) < 0 ? angle[2] + 0.5 : angle[2] - 0.5;
 
-        angle[0] -= (terrain_roll-tmp_roll) > 0 ? angle[0] + 0.5 : angle[0] - 0.5;
-        angle[1] += (terrain_roll-tmp_roll) > 0 ? angle[1] + 0.5 : angle[1] - 0.5;
-        // angle[2] = (terrain_roll-tmp_roll) > 0 ? angle[2] + 0.5 : angle[2] - 0.5;
+        //  m1 += 3*pcval_imu + 2*rcval_imu;
+        //  m2 += 3*pcval_imu - 2*rcval_imu;
+        //  m3 += 2*pcval_imu;
 
-        angle[0] = angle[0] > 90 ? 90 : angle[0] < 0 ? 0 : angle[0];
-        angle[1] = angle[1] > 90 ? 90 : angle[1] < 0 ? 0 : angle[1];
-        angle[2] = angle[2] > 90 ? 90 : angle[2] < 0 ? 0 : angle[2];
+        if(terrain_pitch-tmp_pitch < 0){
+            angle[0] -= 0.2;
+            angle[1] -= 0.2;
+            angle[2] += 0.2;
+        }
+        else{
+            angle[0] += 0.2;
+            angle[1] += 0.2;
+            angle[2] -= 0.2;
+        }
+        if(terrain_roll-tmp_roll < 0){
+            angle[0] += 0.2;
+            angle[1] -= 0.2;
+        }
+        else{
+            angle[0] -= 0.2;
+            angle[1] += 0.2;
+        }
+        
         ESP_LOGD("TAG_SEARCH","time %ld,  %3.2f, %3.2f, %3.2f", (xTaskGetTickCount()-start), angle[0], angle[1], angle[2]);
     }
-    if(target_servo_angle_terrain[0] == -1|| target_servo_angle_terrain[1] == -1|| target_servo_angle_terrain[2] == -1){
+    if(!flag_found){
         ESP_LOGW("TAG", "No solution found");
+        return false;
     }
     else{
         ESP_LOGD("TAG", "Solution found");
-        printf("Solution : %d, %d, %d\n", target_servo_angle_terrain[0], target_servo_angle_terrain[1], target_servo_angle_terrain[2]);
+        ESP_LOGD("TAG", "Solution : %3.2f, %3.2f, %3.2f\n", target_servo_angle_terrain[0], target_servo_angle_terrain[1], target_servo_angle_terrain[2]);
+        return true;
     }
 }
 
@@ -454,6 +471,7 @@ void getTerrainTask(void *arg){
             ESP_LOGW("TAG", "drone already landed or no terrain detected");
         }
         // printf("\n");
+        getFK();
 
         VectorFloat n = getPlaneNorm(legs[0], legs[1], legs[2]);
 
@@ -461,8 +479,10 @@ void getTerrainTask(void *arg){
 
         // pitch and roll
         
-        terrain_pitch = -rad2deg(atan2(n.x, n.z))+180;
-        terrain_roll = -rad2deg(atan2(n.y, n.z));
+        terrain_pitch = rad2deg(atan((n.x)/(n.z)));
+        terrain_roll = rad2deg(atan((n.y)/(n.z)));
+        printf("x/z: %f, y/z: %f\n", (float)((n.x)/(n.z)), (float)((n.y)/(n.z)));
+        
         
         // if(terrain_pitch > 90){
         //     terrain_pitch -= 180;
@@ -472,10 +492,9 @@ void getTerrainTask(void *arg){
         // }
 
         // terrain_pitch = rad2deg(atan2(n.y, n.z)-PI);
-        // terrain_roll = -rad2deg(atan2(n.x, n.z));   
+        // terrain_roll = -rad2deg(atan2(n.x-legs[1].x, n.z-legs[1].z))-180;   
         vTaskDelay(1/portTICK_PERIOD_MS);
         // printf("point: ");
-        getFK();
         searchAngleFK();
 
         } 
@@ -488,20 +507,23 @@ void lidarControlTask(void *pvParameter){
     for (;;){
         vTaskDelayUntil(&lastTime, 200 / portTICK_PERIOD_MS);
         // printf("point: ");
-        for (int i = 0; i < 3; i++)
-        {
-            float angle = getAngle(i + 1);
-            legs[i].x = cos(deg2rad(120 * (i)-60)) * (dBase + L1 * sin(deg2rad(angle)));
-            legs[i].y = sin(deg2rad(120 * (i) - 60)) * (dBase + L1*sin(deg2rad(angle)));
-            if(distance[i] == 0){
-                legs[i].z = z_last[i];
-            }
-            else{
-                legs[i].z = L2 + L1 * cos(deg2rad(angle)) + (distance[i] - dToF);
-                z_last[i] = legs[i].z;
-            }
-            // printf("%d-(%d, %d, %d)\t", i, legs[i].x, legs[i].y, legs[i].z);
-        }
+        int16_t legfx = L1 * sin(deg2rad(getAngle(1))), legfy = L1 * cos(deg2rad(getAngle(1)));
+        legs[0].x = 0.5f * (dBase +legfx);
+        legs[0].y = -0.866025f * (dBase + legfx);
+        legs[0].z = L2 + legfy + (distance[0] - dToF);
+
+        legfx = L1 * sin(deg2rad(getAngle(2)));
+        legfy = L1 * cos(deg2rad(getAngle(2)));
+        legs[1].x = 0.5f * (dBase +legfx);
+        legs[1].y = 0.866025f * (dBase + legfx);
+        legs[1].z = L2 + legfy + (distance[1] - dToF);
+
+        legfx = L1 * sin(deg2rad(getAngle(3)));
+        legfy = L1 * cos(deg2rad(getAngle(3)));
+        legs[2].x = -(dBase +legfx);
+        legs[2].y = 0;
+        legs[2].z = L2 + legfy + (distance[2] - dToF);
+
         if(distance[0] == 0 && distance[1] == 0 && distance[2] == 0){
             pcval_lidar = 0;
             rcval_lidar = 0;
@@ -513,11 +535,12 @@ void lidarControlTask(void *pvParameter){
 
         // pitch and roll
         // pitch and roll
-        terrain_pitch = rad2deg(atan2(n.x,n.z))+180;
-        terrain_roll = -rad2deg(atan2(n.y, n.z));
+        terrain_pitch = rad2deg(atan((n.x)/(n.z)));
+        terrain_roll = rad2deg(atan((n.y)/(n.z)));
 
         pcval_lidar = pid_pitch->update(0, terrain_pitch);
         rcval_lidar = pid_roll->update(0,  terrain_roll);
+
     }
 }
 
@@ -525,7 +548,7 @@ void lidarControlTask(void *pvParameter){
 void servoSweepTask(void *pvParameter){
     // printf("Servo Sweep Task\n");
     // xSemaphoreTakeRecursive(servo_sem, 10/portTICK_PERIOD_MS);
-    for (int i = 0;i<90;i++){
+    for (int i = 0;i<80;i++){
         servo->writeMicroseconds(0,PWM1(i));
         servo->writeMicroseconds(1,PWM2(i));
         servo->writeMicroseconds(2,PWM3(i));
@@ -665,11 +688,14 @@ void buttonInitTask(void *pvParameter){
 }
 
 void suspendedTask(void *pvParameter){
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
     for (;;)
     {
+        xSemaphoreTake(servo_sem, 10/portTICK_PERIOD_MS);
         servo->writeMicroseconds(0, PWM1(60));
         servo->writeMicroseconds(1, PWM2(60));
         servo->writeMicroseconds(2, PWM3(60));
+        xSemaphoreGive(servo_sem);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
@@ -694,15 +720,6 @@ void sendLogTask(void *pvParameter){
         #ifdef CONFIG_LOG_FK
             ESP_LOGI("LOG_FK", "T,x1,y1,z1,x2,y2,z2,x3,y3,z3,pitch,roll");
         #endif
-        #ifdef CONFIG_LOG_LIDAR
-            ESP_LOGI("LOG_LIDAR","T,L1,L2,L3");
-        #endif
-        #ifdef CONFIG_LOG_ADC
-            ESP_LOGI("LOG_ADC","T,a1,a2,a3");
-        #endif  
-        #ifdef CONFIG_LOG_FK
-            ESP_LOGI("LOG_FK", "T,x1,y1,z1,x2,y2,z2,x3,y3,z3,pitch,roll");
-        #endif
     #endif
     TickType_t lastTime = xTaskGetTickCount();
     for (;;)
@@ -711,27 +728,27 @@ void sendLogTask(void *pvParameter){
         #ifdef CONFIG_LOG_DATA_TO_SERIAL
             #ifdef CONFIG_LOG_FK
                 ESP_LOGI("LOG_FK", "%d, %d, %d,\t%d, %d, %d,\t%d, %d, %d,\t%3.2f, %3.2f",  legs[0].x, legs[0].y, legs[0].z, legs[1].x, legs[1].y, legs[1].z, legs[2].x, legs[2].y, legs[2].z, terrain_pitch, terrain_roll);
-                vTaskDelay(10 / portTICK_PERIOD_MS);
+                
             #endif
             #ifdef CONFIG_LOG_LIDAR
-                ESP_LOGI("LOG_LIDAR","%d, %d, %d", distance[0],distance[1],distance[2]);
-                vTaskDelay(10 / portTICK_PERIOD_MS);
+            ESP_LOGI("LOG_LIDAR","%d, %d, %d", distance[0],distance[1],distance[2]);
+                
             #endif
             #ifdef CONFIG_LOG_ADC
                 ESP_LOGI("LOG_ADC","%d, %d, %d", adc_value_filtered[0],adc_value_filtered[1],adc_value_filtered[2]);
-                vTaskDelay(10 / portTICK_PERIOD_MS);
+                
             #endif
             #ifdef CONFIG_LOG_IMU
                 ESP_LOGI("LOG_IMU", "%3.2f, %3.2f, %3.2f",  ypr[0], ypr[1], ypr[2]);
-                vTaskDelay(10 / portTICK_PERIOD_MS);
+                
             #endif
             #ifdef CONFIG_LOG_SERVO    
                 ESP_LOGI("LOG_SERVO", "%d,%d,%d", pwms[0], pwms[1], pwms[2]);
-                vTaskDelay(10 / portTICK_PERIOD_MS);
+                
             #endif
             #ifdef CONFIG_LOG_PID
                 ESP_LOGI("LOG_PID", "%3.2f,%3.2f,%3.2f,%3.2f", pcval_lidar, rcval_lidar, pcval_imu, rcval_imu);
-                vTaskDelay(10 / portTICK_PERIOD_MS);
+                
             #endif
         #endif
         
@@ -752,16 +769,17 @@ void initializeTask(void *pvParameter)
     ESP_LOGD(TAG, "Initializing LiDAR");
     xTaskCreatePinnedToCore(lidarTask, "LiDAR Task", 4096 , NULL, 2, &TaskHandle_lidar, 1);
     ESP_LOGD(TAG, "Initializing ADC");
-    xTaskCreatePinnedToCore(startAdcTask, "adc task", 2048*3, &TaskHandle_ADC, 2, NULL, 1);
-    ESP_LOGD(TAG, "Initializing SUSPEND TASK");
+    xTaskCreatePinnedToCore(startAdcTask, "adc task", 2048*3, &TaskHandle_ADC, 3, NULL, 1);
+    // ESP_LOGD(TAG, "Initializing SUSPEND TASK");
     xTaskCreatePinnedToCore(suspendedTask, "suspended task", 2048, NULL, 2, &TaskHandle_suspended, 1);
     vTaskSuspend(TaskHandle_suspended);
     
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     ESP_LOGD(TAG, "Initializing IMU");
-    xTaskCreatePinnedToCore(imuTask, "IMU Task", 3072, NULL, 2, &TaskHandle_IMU, 1);
+    // xTaskCreatePinnedToCore(imuTask, "IMU Task", 3072, NULL, 2, &TaskHandle_IMU, 1);
+
     ESP_LOGD(TAG, "Starting Control Tasks");
-    xTaskCreatePinnedToCore(imuControlLoopTask, "imu control", 4096, NULL, 1, &TaskHandle_imuControl, 0);
+    // xTaskCreatePinnedToCore(imuControlLoopTask, "imu control", 4096, NULL, 1, &TaskHandle_imuControl, 0);
     
     // xTaskCreatePinnedToCore(lidarControlTask, "lidar control", 4096, NULL, 5, &TaskHandle_lidarControl, 1);
     vTaskDelay(1000/portTICK_PERIOD_MS);
